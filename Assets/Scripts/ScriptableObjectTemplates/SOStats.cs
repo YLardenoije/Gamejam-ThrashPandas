e0a76276ef0fa894d837aafce5888ce4791321d6
yveslardenoije <i369998>
2019-05-10 01:20:07 +0200

    added a ui bars handler

---
+++ Assets/Scripts/ScriptableObjectTemplates/SOStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "SOStats", menuName = "Scriptable Objects/Stats", order = 1)]
+public class SOStats : ScriptableObject
+{
+    public float SliderMaxVal = 100;
+    public UnityEvent happinessChanged = new UnityEvent();
+    public UnityEvent SocialRatingChanged = new UnityEvent();
+    public UnityEvent HungerChanged = new UnityEvent();
+    public float Happines { get; private set; }
+    public float SocialRating { get; private set; }
+    public float Hunger { get; private set; }
+
+    public void addHappiness(int change)
+    {
+        Happines += change;
+        happinessChanged.Invoke();
+    }
+    public void addSocialRatings(int change)
+    {
+        SocialRating += change;
+        SocialRatingChanged.Invoke();
+    }
+    public void addHunger(int change)
+    {
+        Hunger += change;
+        HungerChanged.Invoke();
+    }
+    public void ClearValues()
+    {
+        Hunger = 0.5f*SliderMaxVal;
+        SocialRating = 0.5f * SliderMaxVal;
+        Happines = 0.5f * SliderMaxVal;
+        happinessChanged = new UnityEvent();
+        SocialRatingChanged = new UnityEvent();
+        HungerChanged = new UnityEvent();
+    }
+
+
+}
---
+++ Assets/Scripts/UI/UIStatHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UIStatHandler : MonoBehaviour
+{
+    public SOStats Stats;
+    [SerializeField] private Slider HappinessMeter;
+    [SerializeField] private Slider HungerMeter;
+    [SerializeField] private Slider SocializationMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stats.happinessChanged.AddListener(UpdateBars);
+        Stats.HungerChanged.AddListener(UpdateBars);
+        Stats.SocialRatingChanged.AddListener(UpdateBars);
+
+    }
+    void UpdateBars()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
